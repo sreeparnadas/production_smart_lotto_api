8d668cefd4e2ce9e27c8fae7586355f4c2842757
sreeparnadas <sreeparnadas0675@gmail.com>
2021-12-03 15:59:20 +0530

    card submit

--- app/Http/Controllers/PlayController.php
+++ app/Http/Controllers/PlayController.php
@@ -7,75 +7,95 @@ use App\Http\Resources\PlayDetailsResource;
 use App\Http\Resources\PlayMasterResource;
 use App\Http\Resources\PrintSingleGameInputResource;
 use App\Http\Resources\PrintTripleGameInputResource;
+use App\Models\CardPlayDetail;
+use App\Models\CardPlayMaster;
 use App\Models\GameType;
 use App\Models\PlayDetails;
 use App\Models\PlayMaster;
 use App\Models\SingleNumber;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class PlayController extends Controller
 {
     public function save_play_details(Request $request)
     {
         $requestedData = $request->json()->all();
-        $validator = Validator::make($requestedData,[
-            'playMaster' => 'required',
-            'playDetails' => 'required'
-        ]);
 
-        if($validator->fails()){
-            return response()->json(['position'=>1,'success'=>0,'data'=>null,'error'=>$validator->messages()], 406,[],JSON_NUMERIC_CHECK);
-        }
+//        return response()->json(['success'=>1,'data'=> $requestedData], 200,[],JSON_NUMERIC_CHECK);
+//        $validator = Validator::make($requestedData,[
+//            'playMaster' => 'required',
+//            'playDetails' => 'required'
+//        ]);
+//
+//        if($validator->fails()){
+//            return response()->json(['position'=>1,'success'=>0,'data'=>null,'error'=>$validator->messages()], 406,[],JSON_NUMERIC_CHECK);
+//        }
         $inputPlayMaster = (object)$requestedData['playMaster'];
         $inputPlayDetails = $requestedData['playDetails'];
 
-        //        Validation for PlayMaster
-        $rules = array(
-            'drawMasterId'=>'required|exists:draw_masters,id',
-            'terminalId'=> ['required',
-                function($attribute, $value, $fail){
-                    $terminal=User::where('id', $value)->where('user_type_id','=',4)->first();
-                    if(!$terminal){
-                        return $fail($value.' is not a valid terminal id');
-                    }
-                }],
-        );
-        $messages = array(
-            'drawMasterId.required'=>'Draw time is required',
-            'terminalId.required'=>'Terminal Id is required',
-        );
 
-        $validator = Validator::make($requestedData['playMaster'],$rules,$messages );
-
-        if ($validator->fails()) {
-            return response()->json(['position'=>2,'success'=>0,'data'=>null,'error'=>$validator->messages()], 406,[],JSON_NUMERIC_CHECK);
-        }
-        //        Validation for PlayMaster complete
-
-
-        //validation for playDetails
-        $rules = array(
-            "*.gameTypeId"=>'required|exists:game_types,id',
-            // '*.singleNumberId' => 'required_if:*.gameTypeId,==,1',
-            '*.quantity' => 'bail|required|integer|min:1',
-            '*.mrp' => 'bail|required|integer|min:1'
-        );
-        $validator = Validator::make($requestedData['playDetails'],$rules );
-        if ($validator->fails()) {
-            return response()->json(['position'=>3,'success'=>0,'data'=>null,'error'=>$validator->messages()], 406,[],JSON_NUMERIC_CHECK);
-        }
+//        return response()->json(['success'=>1,'data'=> $inputPlayMaster], 200,[],JSON_NUMERIC_CHECK);
+//
+//        //        Validation for PlayMaster
+//        $rules = array(
+//            'drawMasterId'=>'required|exists:draw_masters,id',
+//            'terminalId'=> ['required',
+//                function($attribute, $value, $fail){
+//                    $terminal=User::where('id', $value)->where('user_type_id','=',4)->first();
+//                    if(!$terminal){
+//                        return $fail($value.' is not a valid terminal id');
+//                    }
+//                }],
+//        );
+//        $messages = array(
+//            'drawMasterId.required'=>'Draw time is required',
+//            'terminalId.required'=>'Terminal Id is required',
+//        );
+//
+//        $validator = Validator::make($requestedData['playMaster'],$rules,$messages );
+//
+//        if ($validator->fails()) {
+//            return response()->json(['position'=>2,'success'=>0,'data'=>null,'error'=>$validator->messages()], 406,[],JSON_NUMERIC_CHECK);
+//        }
+//        //        Validation for PlayMaster complete
+//
+//
+//        //validation for playDetails
+//        $rules = array(
+//            "*.gameTypeId"=>'required|exists:game_types,id',
+//            // '*.singleNumberId' => 'required_if:*.gameTypeId,==,1',
+//            '*.quantity' => 'bail|required|integer|min:1',
+//            '*.mrp' => 'bail|required|integer|min:1'
+//        );
+//        $validator = Validator::make($requestedData['playDetails'],$rules );
+//        if ($validator->fails()) {
+//            return response()->json(['position'=>3,'success'=>0,'data'=>null,'error'=>$validator->messages()], 406,[],JSON_NUMERIC_CHECK);
+//        }
         //end of validation for playDetails
 
-        $output_array = array();
+//        $output_array = array();
+
+//        $playMaster = new CardPlayMaster();
+//        $playMaster->card_draw_master_id = $inputPlayMaster->drawMasterId;
+//        $playMaster->barcode_number = "dsdasd";
+//        $playMaster->user_id = $inputPlayMaster->terminalId;
+//        $playMaster->save();
+//        return response()->json(['success'=>1,'data'=> $playMaster], 200,[],JSON_NUMERIC_CHECK);
+
+//        $quantity = 0;
+//        $amount = 0;
 
         DB::beginTransaction();
         try{
 
-            $playMaster = new PlayMaster();
-            $playMaster->draw_master_id = $inputPlayMaster->drawMasterId;
+            $playMaster = new CardPlayMaster();
+            $playMaster->card_draw_master_id = $inputPlayMaster->drawMasterId;
+//            $playMaster->barcode_number = "dsdasdr";
             $playMaster->user_id = $inputPlayMaster->terminalId;
             $playMaster->save();
             $output_array['play_master'] = new PlayMasterResource($playMaster);
@@ -83,14 +103,15 @@ class PlayController extends Controller
             $output_play_details = array();
             foreach($inputPlayDetails as $inputPlayDetail){
                 $detail = (object)$inputPlayDetail;
+//                $quantity = $quantity + $inputPlayDetail->quantity;
                 if($detail->gameTypeId) {
                     $gameType = GameType::find($detail->gameTypeId);
                     //insert value for triple
 //                    if ($detail->gameTypeId == 1) {
-                        $playDetails = new PlayDetails();
-                        $playDetails->play_master_id = $playMaster->id;
+                        $playDetails = new CardPlayDetail();
+                        $playDetails->card_play_master_id = $playMaster->id;
                         $playDetails->game_type_id = $detail->gameTypeId;
-                        $playDetails->two_digit_number_set_id = $detail->twoDigitNumberSetId;
+                        $playDetails->card_combination_id = $detail->cardCombinationId;
                         $playDetails->quantity = $detail->quantity;
                         $playDetails->mrp = $detail->mrp;
                         $playDetails->commission = $gameType->commission;
@@ -104,17 +125,20 @@ class PlayController extends Controller
                 }
 
             }
-            $output_array['game_input'] = $this->get_game_input_details_by_play_master_id($playMaster->id);
+//            $output_array['game_input'] = $this->get_game_input_details_by_play_master_id($playMaster->id);
 
 
             $amount = $playMaster->play_details->sum(function($t){
                 return $t->quantity * $t->mrp;
             });
-            $output_array['amount'] = round($amount,0);
+
+//            $amount =
 
             $terminal = User::findOrFail($inputPlayMaster->terminalId);
             $terminal->closing_balance-= $amount;
             $terminal->save();
+
+
 
             DB::commit();
         }catch (\Exception $e){
@@ -122,7 +146,7 @@ class PlayController extends Controller
             return response()->json(['success'=>0,'exception'=>$e->getMessage(),'error_line'=>$e->getLine(),'file_name' => $e->getFile()], 500);
         }
 
-        return response()->json(['success'=>1,'data'=> $output_array], 200,[],JSON_NUMERIC_CHECK);
+        return response()->json(['success'=>1,'data'=> $amount], 200,[],JSON_NUMERIC_CHECK);
     }
 
     public function get_game_input_details_by_play_master_id($play_master_id){
--- app/Models/CardPlayMaster.php
+++ app/Models/CardPlayMaster.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Webpatser\Uuid\Uuid;
 
 class CardPlayMaster extends Model
 {
     use HasFactory;
+
+    protected static function boot()
+    {
+        parent::boot();
+        CardPlayMaster::saving(function ($model) {
+            $model->barcode_number = str_replace('-','x', ((string)Uuid::generate()));
+        });
+    }
+
+    public function play_details(){
+        return $this->hasMany(CardPlayDetail::class,'card_play_master_id');
+    }
 }
