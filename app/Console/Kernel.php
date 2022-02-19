ffc86a167d2eff53118300cd846668bdc802c7db
sreeparnadas <sreeparnadas0675@gmail.com>
2022-02-19 14:08:14 +0530

    kernel updated

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         '\App\Console\Commands\GenerateResult',
+        '\App\Console\Commands\CardGenerateResult',
     ];
 
     /**
